5ae458c6e77aa72b2b0912f30c3142bbf741e530
sure srikanth <suresrikanth101@gmail.com>
2023-05-02 14:37:42 +0530

    fixed AB#2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h1>Generate inferences from ML model</h1>
+    <h1>Generate inferences from ML model 101</h1>
 	<h2>Upload your data to make to get predictions</h2>
     <img
       alt="Vue logo"
